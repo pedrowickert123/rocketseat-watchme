65d6112a6845d6b6bf92dce6a9c8599c3e96abd2
Pedro Wickert <pedrowickert98@gmail.com>
2021-07-22 10:32:32 -0300

    Challenge Changes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import React from "react";
+
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: GenreProps[];
+  onClick: (id: number) => void;
+}
+
+export function SideBar({ selectedGenreId, genres, onClick }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
